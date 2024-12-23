ad38dd35dd2ae23b90991a457c0116e9af7e731e
bhagyashree9025 <bhagyashree9025@gmail.com>
2024-12-23 15:46:37 +0530

    Lazy loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import "./App.css";
-import Header from "./Components/Header";
-import Body from "./Components/Body";
+// import './App.css'
+// import Header from "./Components/Header";
+// import Body from "./Components/Body";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Body />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <Body />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 import Header from './Components/Header'
-import AboutUs from './Components/AboutUs'
+// import AboutUs from './Components/AboutUs'
 import ContactUs from './Components/ContactUs'
 import Cart from './Components/Cart'
 import Error from './Components/Error'
 import Body from './Components/Body'
 import RestaurantMenu from './Components/RestaurantMenu'
+// import Grocery from './Components/Grocery'
+const AboutUs = lazy(() => import('./Components/AboutUs'))
+const Grocery = lazy(() => import('./Components/Grocery'))
 
 function AppLayout() {
     return (
@@ -26,9 +28,26 @@ const appRouter = createBrowserRouter([
         element: <AppLayout />,
         children: [
             { path: '/', element: <Body /> },
-            { path: '/about', element: <AboutUs /> },
+            {
+                path: '/about',
+                element: <AboutUs />,
+                element: (
+                    <Suspense fallback={<h1>Loading....</h1>}>
+                        <AboutUs />
+                    </Suspense>
+                ),
+            },
             { path: '/contact', element: <ContactUs /> },
             { path: '/cart', element: <Cart /> },
+            {
+                path: '/grocery',
+                element: <Grocery />,
+                element: (
+                    <Suspense fallback={<h1>Loading....</h1>}>
+                        <Grocery />
+                    </Suspense>
+                ),
+            },
             { path: '/restaurants/:resId', element: <RestaurantMenu /> },
         ],
         errorElement: <Error />,
